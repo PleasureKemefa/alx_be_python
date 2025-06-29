f7d6445e0aec43ce503a04da07bd8e0440e48e98
PleasureKemefa <80639745+PleasureKemefa@users.noreply.github.com>
2025-06-29 07:41:40 -0700

    Update class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,22 +1,12 @@
-import math
 
-class Shape:
-    def area(self):
-        raise NotImplementedError("Subclasses must override the area() method.")
+class Calculator:
+    calculation_type = "Arithmetic Operations"
 
+    @staticmethod
+    def add(a, b):
+        return a + b
 
-class Rectangle(Shape):
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width
-
-
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * (self.radius ** 2)
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
